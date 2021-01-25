7a3712689196bb2d6606aa6e844102aafa507259
= <lopke123@live.nl>
2021-01-25 05:32:41 +0100

    fixed VS duck-ups

---
+++ Examples/U8Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace U8Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("U8Disasm Console Example.");
+
+            // Insert your ROM file here
+            Example test = new Example(@"L:\Projects\Calculator\Casio\ROM_Dump.mem");
+            test.Disassemble(LineCount: 2000, PrintLines: true);
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
